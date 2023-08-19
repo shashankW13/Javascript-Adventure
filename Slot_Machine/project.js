51648508f68e05450f5066c296980d757997f633
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-08-19 19:19:16 +0530

    Added spin function

--- Slot_Machine/project.js
+++ Slot_Machine/project.js
@@ -1,5 +1,22 @@
 const prompt = require("prompt-sync")()
 
+const ROWS = 3
+const COLS = 3
+
+const SYMBOL_COUNT = {
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8,
+}
+
+const SYMBOL_VALUES = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+}
+
 const deposit = () => {
   while (true) {
     const depositAmount = prompt("Enter the amount you want to deposit: ")
@@ -33,8 +50,35 @@ const getBet = (balance, lines) => {
   }
 }
 
-let balance = deposit()
-const numberOfLines = getNumberOfLines()
-const bet = getBet(balance, numberOfLines)
-console.log(balance)
-console.log(numberOfLines)
+const spin = () => {
+  const symbols = []
+
+  for (const [symbol, count] of Object.entries(SYMBOL_COUNT)) {
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol)
+    }
+  }
+
+  const reels = [[], [], []]
+
+  for (let i = 0; i < COLS; i++) {
+    const reelSymbols = [...symbols]
+    console.log(reelSymbols)
+    for (let j = 0; j < ROWS; j++) {
+      const randomIndex = Math.floor(Math.random() * reelSymbols.length)
+      const selectedSymbol = reelSymbols[randomIndex]
+      reels[i].push(selectedSymbol)
+      reelSymbols.splice(randomIndex, 1)
+    }
+  }
+
+  return reels
+}
+
+const reels = spin()
+console.log(reels)
+// let balance = deposit()
+// const numberOfLines = getNumberOfLines()
+// const bet = getBet(balance, numberOfLines)
+// console.log(balance)
+// console.log(numberOfLines)
